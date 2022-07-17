58fe2c221b7a34dac4e73102b77f9fc229a0d94b
Abbos <you@example.com>
2022-07-17 22:09:50 +0500

    added requirements.txt

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='posts')
-
+    path('', PostListView.as_view(), name='posts'),
+    path('<int:pk>/post/', PostDetailView.as_view(), name='detail')
 ]
