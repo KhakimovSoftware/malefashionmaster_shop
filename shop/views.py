9174fc2351e9411bbbb8b2a6c6708d4e1584a954
Abbos <you@example.com>
2022-07-21 12:00:10 +0500

    over project5

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ProductModel
+
+
+class ShopView(ListView):
+    template_name = 'shop.html'
+
+    def get_queryset(self):
+        return ProductModel.objects.all()
