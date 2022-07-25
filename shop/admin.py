d9a95275b47fa76a2243f29fb2edbc642e28dee4
Abbos <you@example.com>
2022-07-25 19:01:19 +0500

    big changes

--- shop/admin.py
+++ shop/admin.py
@@ -1,12 +1,40 @@
 from django.contrib import admin
-from .models import ProductTagModel, ProductModel, CategoryModel
+from django.utils.safestring import mark_safe
 
+from .models import ProductTagModel, ProductModel, CategoryModel, SizeModel, ColorModel, BrandModel
+from .forms import ColorModelAdminForms
 
 @admin.register(ProductTagModel)
 class ProductTagModelAdmin(admin.ModelAdmin):
     list_display = ['name']
     list_display_links = ['name']
     search_fields = ['name']
+
+
+@admin.register(SizeModel)
+class SizeModelAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+
+@admin.register(BrandModel)
+class BrandModelAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+
+@admin.register(ColorModel)
+class ColorModelAdmin(admin.ModelAdmin):
+    list_display = ['code', 'color']
+    list_display_links = ['code']
+    search_fields = ['code']
+    form = ColorModelAdminForms
+
+    def color(self, obj):
+        free_space = '&nbsp;' * 5
+        return mark_safe(f"<div style='background-color:{obj.code}; width:200px;'>{free_space}</div>")
 
 
 @admin.register(CategoryModel)
@@ -22,4 +50,4 @@ class ProductModelAdmin(admin.ModelAdmin):
     list_display_links = ['title', 'price', 'created_at']
     list_filter = ['created_at']
     search_fields = ['title', 'price']
-    autocomplete_fields = ['category', 'tags']
+    autocomplete_fields = ['category', 'tags', 'sizes', 'colors']
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,6 @@
-
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import ProductModel
+from django.views.generic import ListView, DetailView
+from .models import ProductModel, CategoryModel, ProductTagModel, ColorModel, BrandModel, SizeModel
 
 
 class ShopView(ListView):
@@ -9,4 +8,55 @@ class ShopView(ListView):
     paginate_by = 3
 
     def get_queryset(self):
-        return ProductModel.objects.all()
+        qs = ProductModel.objects.all()
+
+        search = self.request.GET.get('search')
+        if search:
+            qs = qs.filter(title__icontains=search)
+
+        cat = self.request.GET.get('cat')
+        if cat:
+            qs = qs.filter(category_id=cat)
+
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tags=tag)
+
+        size = self.request.GET.get('size')
+        if size:
+            qs = qs.filter(sizes=size)
+
+        color = self.request.GET.get('color')
+        if color:
+            qs = qs.filter(colors=color)
+
+        brand = self.request.GET.get('brand')
+        if brand:
+            qs = qs.filter(brand_id=brand)
+
+        sort = self.request.GET.get('sort')
+        if sort == 'price':
+            qs = qs.order_by('price')
+        elif sort == '-price':
+            qs = qs.order_by('-price')
+
+        return qs
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data()
+        data['categories'] = CategoryModel.objects.all()
+        data['tags'] = ProductTagModel.objects.all()
+        data['sizes'] = SizeModel.objects.all()
+        data['brands'] = BrandModel.objects.all()
+        data['colors'] = ColorModel.objects.all()
+        return data
+
+
+class ProductDetailView(DetailView):
+    model = ProductModel
+    template_name = 'shop-details.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data()
+        data['products'] = ProductModel.objects.all().exclude(id=self.object.pk)[:4]
+        return data
